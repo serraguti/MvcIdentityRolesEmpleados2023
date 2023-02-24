4b4928b42c2356d06289da104d0ff5a17f09c13b
serraguti <pacoserranox@gmail.com>
2023-02-24 18:02:58 +0100

    Add project files.

---
+++ MvcIdentityRolesEmpleados/Data/EmpleadosContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcIdentityRolesEmpleados.Models;
+
+namespace MvcIdentityRolesEmpleados.Data
+{
+    public class EmpleadosContext: DbContext
+    {
+        public EmpleadosContext(DbContextOptions<EmpleadosContext> options)
+            : base(options) { }
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
---
+++ MvcIdentityRolesEmpleados/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,44 @@
+﻿using MvcIdentityRolesEmpleados.Data;
+using MvcIdentityRolesEmpleados.Models;
+
+namespace MvcIdentityRolesEmpleados.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private EmpleadosContext context;
+
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            return this.context.Empleados.ToList();
+        }
+
+        public Empleado FindEmpleado(int id)
+        {
+            return
+                this.context.Empleados.FirstOrDefault(z => z.IdEmpleado == id);
+        }
+
+        public List<Empleado> GetEmpleadosDepartamento(int iddepartamento)
+        {
+            return
+                this.context.Empleados.Where(z => z.Departamento == iddepartamento)
+                .ToList();
+        }
+
+        //NECESITAMOS UN METODO PARA COMPROBAR SI EXISTE UN EMPLEADO
+        //POR SU USUARIO (APELLIDO) Y PASSWORD (EMP_NO)
+        public Empleado ExisteEmpleado(string apellido, int idempleado)
+        {
+            var consulta = from datos in this.context.Empleados
+                           where datos.Apellido == apellido
+                           && datos.IdEmpleado == idempleado
+                           select datos;
+            return consulta.FirstOrDefault();
+        }
+    }
+}
